31f6483a59febc1ee2855ef4c45398f3817ec5ec
rkritchat <rkritchat@gmail.com>
2020-01-19 21:53:44 +0700

    initial project

---
+++ src/main/java/com/ep/registersv/RegisterSvApplication.java
@@ -0,0 +1,13 @@
+package com.ep.registersv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterSvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ep/registersv/RegisterSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ep.registersv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
